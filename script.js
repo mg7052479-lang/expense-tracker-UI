3cdfa6009c5faef7a109f195c781262ef41a34ca
Mohammed Ghouse. <mg7052479@gmail.com>
2026-01-18 23:56:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,9 +10,34 @@
    const type = document.getElementById("type")
    const addBtn = document.getElementById("addbtn")
 
-//   /// Initiling the arrays and variable in expense and income .. 
+  /// Initiling the arrays and variable in expense and income .. 
 
-  let expense = [];
+  let expenses = [];
   let income = [];
   let totalExpense = 0;
   let totalIncome = 0;
+
+ // Add event listner to the add button  .. 
+
+ addBtn.addEventListener("click", function () {
+     // Create an object to represent the expnese and the amount ... 
+   let dataValues = {                                      
+                      Discription:expenseDescription.nodeValue,     // Must be a douts 2// 
+                      Amount:expenseAmount.value, 
+                      type:type.value
+   };
+     // if the expenses has the dispriction and amount , add it to the appropriate array ..   
+   if (dataValues.Discription && dataValues.Amount){
+      if (dataValues.type === "expense"){
+         expenses.push(dataValues);
+         totalExpense +=dataValues.Amount ;
+      }
+      else {
+         income.push(dataValues);
+         totalIncome += dataValues.Amount
+      }
+   }
+
+ })
+
+ // create a Function to a
